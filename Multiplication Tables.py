827c1dc71cf7c59dd7ba6b438ac78f7055e9c121
Mohammad Areeb Akhter <60102002+mohammad-areeb@users.noreply.github.com>
2020-01-23 17:59:10 +0530

    Add files via upload
    Not much, but some interesting things.

---
+++ Multiplication Tables.py
@@ -0,0 +1,12 @@
+i=1
+e=int(input("enter the number whose table you want to find:"))
+d=0
+n=int(input("enter till what number you want to find the table"))
+while (i<n) : 
+    print("{} into {}".format(e,i))
+    d=e*i
+    print(d)
+    i=i+1
+for i in [5, 0]:
+    print(i+1)
+y=input("enter yes to end the progam")
---
+++ guessing game.py
@@ -0,0 +1,57 @@
+
+print("FOLOW THE RULES AND DO NOT TRY TO ACT SMART.")
+r=input("Liked my new python skills? type yes to start: ")
+e=r.find('yes')
+if (e!=-1):
+    s=input("Confirm your identity by stating the code name by which you are usually called upon: ")
+    a=s.find('#name3')
+    b=s.find('#name2')
+    c=s.find('#name1')
+    if(a!=-1 or b!=-1 or c!=-1):
+        print("Lets see how far you can hack into and get the programmer's secrets out without getting locked out.")
+        print("the secrets are in the form of the questions, the more you got right, the deeper you will go.\n so now we start. ")
+        t=input("#question \n ")
+        d=t.find('#answer')
+        if(d!=-1):
+            print("Good job. So far so right.")
+            u=input("#question ")
+            f=u.find('#answer')
+            if(f!=-1):
+                print("Well done.")
+                v=input("What is the name of the best student magzine?\n")
+                g=v.find('#answer')
+                if(g!=-1):
+                    print("Nice.")
+                    zu=input("Time for a new trick up my sleeve,Type something for confirmation- ")
+                    from PIL import Image
+                    myImage = Image.open('#file_location')
+                    myImage.show()
+                    w=int(input("#question \n"))
+                    if(w==6):
+                        print("great going.")
+                        for q in range(h):
+                            x=int(input("So choose- "))
+                            if (x==1):
+                                print("#something1")
+                            if (x==2):
+                                print("#something2\n")
+                            if (x==3):
+                                print("#something3\n")
+                            if (x==4):
+                                print("#something4\n ")
+                            else:
+                                print("Keep Experimenting.")
+                        print("Thats the end of this program and beginning of new start.\n")
+                        ar=input("i forgot to show you something else. Do you love me? i already know its a yes\n just tell my program also its that- ")
+                    else:
+                        so=input("security compromised\n SERVER LOCKDOWN")
+                else:
+                    som=input("security compromised\n SERVER LOCKDOWN")
+            else:
+                some=input("security compromised\n SERVER LOCKDOWN")
+        else:
+            somet=input("security compromised\n SERVER LOCKDOWN")
+    else:
+        someth=input("security compromised\n SERVER LOCKDOWN")
+else:
+    somethi=input("security compromised\n SERVER LOCKDOWN")
